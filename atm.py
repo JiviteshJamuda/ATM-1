3cdbad2d8ef6f251121f6f8940aced154d468b21
JiviteshJamuda <jivitesh2006@gmail.com>
2020-10-21 23:19:40 +0530

    pay-ATM करो

---
+++ atm.py
@@ -0,0 +1,35 @@
+money = input("Enter the amount to be Withdrawn : ")
+
+# Creating the class namely Atm
+class Atm(object):
+    # Hastag constructor 
+    def __init__(self, atm_card_number, pin_number):
+        self.atm_card_number = atm_card_number
+        self.pin_number = pin_number
+    
+    # Function/method for withdrawing something
+    def cashWithdrawl(self):
+        print("₹" + money + " : Has been withdrawn successfully from you bank account")
+    
+    # Another function/method for annoying the person reading the comments/ I don't know why you want to waste your time reading this useless thing because this thing is written after the hastag thing which is also a snytax or anything which is used for writing comments/sentences in green letters which do not have any effect on our code that we have written and if you are still reading this thing then feel free to give yourself a pat on the back because you have wasted a lot of time reading this comment/green letters
+    def balanceEnquiry(self):
+        print("Your total balance of your account is = ₹" + string)
+    
+# Creating a variable for making the object of class Atm
+account = Atm(12345,67890)
+# Calling both of the functions/methods
+# Please ignore the mess I made with the variables just go on for the output
+account.cashWithdrawl()
+
+money2 = int(money)
+balance = 10000000
+total = balance - money2
+string = str(total)
+
+account.balanceEnquiry()
+
+# Now stop reading my comments/green letters and go get some health
+# this definitely shows NINJA : /\/ / /\/ _/ /\
+
+
+# Minecraft will show its rule again
